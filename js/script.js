624da25cf733fc37ef62d4510b3a1abdb67a985b
provash10 <provash20cb@gmail.com>
2024-11-26 01:10:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+for (let i=1; i<=10; i++){
+    console.log("Hello Wold");
+    console.log(i);
+};
+
+
+// Array 
+let marks = [23, 67, 89, 99, 33];
+
+// marks[0];
+// marks[1];
+// marks[2];
+// marks[3];
+// marks[4];
+
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+
+
+// Array with loop
+
+for (let i=0; i<=4; i++){
+    // console.log(marks[0]);
+    console.log(marks[i]);
+}
+
+
+let number = 20;
+
+if(number%2 == 0){
+    console.log("Even Numver");
+}
